cd332149eda973838069b214dc896670a664128d
zehraBal <114819358+zehraBal@users.noreply.github.com>
2024-07-15 11:17:36 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="/src/Assets/mile1-assets/logo.svg" alt="logo" />
+      <img src="/mile1-assets/logo.svg" alt="logo" />
     </header>
   );
 }
